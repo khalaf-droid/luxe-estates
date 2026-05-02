91d1fb1a19ec7d211b386354fce0058c6d8699cf
Khalaf Hussein Khalaf Hussein <145412532+khalaf-droid@users.noreply.github.com>
2026-05-03 02:27:46 +0300

    resolve merge conflicts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,21 +6,6 @@
 
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  apiUrl: 'http://localhost:5001/api/v1',
-  googleClientId: 'your_google_client_id_here'
-=======
-<<<<<<< Updated upstream
   apiUrl: 'http://localhost:5002/api/v1',
   googleClientId: '703833352527-f08lpsbitb01npni81tlvehmtum1kh71.apps.googleusercontent.com'
-=======
-<<<<<<< HEAD
-  apiUrl: 'http://localhost:5001/api/v1',
-  googleClientId: 'your_google_client_id_here'
-=======
-  apiUrl: 'http://localhost:5002/api/v1',
-  googleClientId: '703833352527-f08lpsbitb01npni81tlvehmtum1kh71.apps.googleusercontent.com'
->>>>>>> feature/auth-update
->>>>>>> Stashed changes
->>>>>>> feature/auth-update
 };
